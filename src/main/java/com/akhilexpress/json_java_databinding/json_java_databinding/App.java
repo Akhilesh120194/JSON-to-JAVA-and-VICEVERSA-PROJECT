d1a330967fbb223d49b9ec2f96283bb1d3dba5a5
Akhilesh120194 <akhileshsinghtomar15@gmail.com>
2022-04-30 16:06:09 +0530

    Json to JAVA and ViceVersa  project

---
+++ src/main/java/com/akhilexpress/json_java_databinding/json_java_databinding/App.java
@@ -0,0 +1,13 @@
+package com.akhilexpress.json_java_databinding.json_java_databinding;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
